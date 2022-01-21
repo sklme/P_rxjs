71d7f4c9c91f00aadc09f49c0c5870b53e62ffb6
wrynnsun <wrynnsun@tencent.com>
2022-01-21 22:02:55 +0800

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+    extraFileExtensions: [".cjs"],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  ignorePatterns: [".eslintrc.cjs", "tsconfig.json"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unsafe-return": "warn",
+        "@typescript-eslint/no-unsafe-call": "warn",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-unsafe-argument": "warn",
+        "@typescript-eslint/no-unsafe-member-access": "warn",
+      },
+    },
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import log from "./util/log.js";
+log();
