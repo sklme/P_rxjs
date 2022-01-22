24764b3025764f58b8e6c87fbacd2061dd40ecdc
skl <sun.keliang@foxmail.com>
2022-01-22 22:16:41 +0800

    feat(observable): Executing Observables

--- src/obeservables/index.ts
+++ src/obeservables/index.ts
@@ -1,5 +1,6 @@
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 
+// 第一个例子
 (function () {
   const observable = new Observable<number>((subscribe) => {
     subscribe.next(1);
@@ -27,3 +28,46 @@ import { Observable } from 'rxjs';
     },
   });
 })();
+
+// executing observables
+(function () {
+  // next and complete
+  const observable1 = new Observable<number>(function subscribe(subscriber) {
+    //
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    subscriber.complete();
+    subscriber.next(4); // not reached
+  });
+
+  observable1.subscribe((x) => console.log(`next and complete`, x));
+
+  // next and error
+  const observable2 = new Observable<number>(function subscribe(subscriber) {
+    //
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    subscriber.error('一个巨大的错误发生了');
+    subscriber.next(4); // not reached
+  });
+
+  observable2.subscribe({
+    next(x) {
+      console.log('next and error', x);
+    },
+    error(err) {
+      console.log(err);
+    },
+  });
+})();
+
+// (() => {
+//   // Disposing Obervable Executions
+//   const observable = from([10, 20, 30]);
+//   const subscription = observable.subscribe((x) =>
+//     console.log('Disposing Obervable Executions', x),
+//   );
+//   subscription.unsubscribe();
+// })();
