9d7518463d0bb578e2977f984fd47649d7cf37cb
wrynnsun <wrynnsun@tencent.com>
2022-01-27 18:28:22 +0800

    feat(buffer): TypeBuffer构造函数

--- src/buffer/arrayBuffer.ts
+++ src/buffer/arrayBuffer.ts
@@ -42,6 +42,20 @@ import { makeTitle } from '../util/makeTitle';
  */
 (() => {
   makeTitle('TypeBuffer构造函数');
+  makeTitle('使用arrayBuffer构造');
+  // 使用arrayBuffer构造
+  const buf = new ArrayBuffer(32);
+  const i32View = new Int32Array(buf);
+  console.log(i32View.byteLength); // 32
+  console.log(i32View.length); // 8
+  const i32ViewWithOffset = new Int32Array(buf, 4); // 传递byteoffet
+  console.log(i32ViewWithOffset.byteLength); // 28
+  console.log(i32ViewWithOffset.length); // 7
+  const i32ViewWithOffsetAndLength = new Int32Array(buf, 4, 2);
+  console.log(i32ViewWithOffsetAndLength.byteLength); // 8
+  console.log(i32ViewWithOffsetAndLength.length); // 2
+
+  makeTitle('使用arrayLike构造');
   // 使用 arrayLike 构造
   // 数组
   const int8View = new Int8Array([1, 2, 3]);
@@ -56,4 +70,15 @@ import { makeTitle } from '../util/makeTitle';
   console.log(int16View);
   console.log(int16View.length); // 3
   console.log(int16View.byteLength); // 6
+
+  makeTitle('使用数值构造');
+  const f64a = new Float64Array(3); // 传入的数值是成员的length
+  f64a[0] = 10;
+  f64a[1] = 30;
+  f64a[2] = f64a[0] + f64a[1];
+  console.log(f64a[2]); // 40
+  console.log(f64a.byteLength); // 24
+  console.log(f64a.length); // 3
 })();
+
+//
